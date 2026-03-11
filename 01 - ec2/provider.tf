2657279834cb2692b53cd03348b7531830aca8ee
chaitanya <vamsi.blaze1@gmail.com>
2026-03-11 23:20:58 +0530

    updated provider.tf file

--- 01 - ec2/provider.tf
+++ 01 - ec2/provider.tf
@@ -1,11 +1,28 @@
 terraform {
   required_providers {
     aws = {
-      source = "value"
-      version = "value"
-          }
+      source  = "hashicorp/aws"
+      version = "6.16.0"
+    }
   }
 }
+
 provider "aws" {
   region = "us-east-1"
 }
+
+# -----------------------------
+# Concept Explanation:
+# In Terraform, a "provider" is a plugin that allows Terraform
+# to interact with a specific cloud platform or service (like AWS, Azure, or GCP).
+# It defines how Terraform should connect, authenticate, and manage resources
+# on that platform.
+
+# In this example:
+# - The AWS provider is being used.
+# - Terraform will deploy resources in the "us-east-1" region.
+
+# This is a core concept in Terraform and every project needs at least one provider.
+# Understanding providers is essential for beginners and anyone learning
+# Infrastructure as Code with Terraform.
+# -----------------------------
