f9f52dbe4abf7ddfe174843a2805847402619199
chaitanya <vamsi.blaze1@gmail.com>
2026-03-12 17:34:49 +0530

    added comments for output.tf

--- 04 - loops/01 - count/outputs.tf
+++ 04 - loops/01 - count/outputs.tf
@@ -1,3 +1,6 @@
 output "sample_output" {
   value = aws_instance.web_server
 }
+
+# Output values are used to display useful information after Terraform creates the infrastructure.
+# This helps users see details such as instance IDs, IP addresses, or other resource attributes.
