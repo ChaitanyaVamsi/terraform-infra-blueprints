8ef886422af8616b3957dc16cb4d50e7122a1313
chaitanya <vamsi.blaze1@gmail.com>
2026-03-17 13:19:27 +0530

    feature added provider,main,variables for sg

---
+++ 20 - jenkins-terraform-infra/10-sg/main.tf
@@ -0,0 +1,9 @@
+module "sg" {
+  count = lenght(var.sg_names)
+  source = "git::https://github.com/daws-86s/terraform-aws-sg.git?ref=main"
+  project_name = var.project_name
+  environtment = var.environment
+  sg_name = var.sg_names[count.index]
+  sg_description = "Created for ${var.sg_names[count.index]}"
+  vpc_id = local.vpc_id
+}
