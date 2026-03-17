4d3612000294a6c8119f63cc89c5fa53e999062e
chaitanya <vamsi.blaze1@gmail.com>
2026-03-17 17:23:23 +0530

    feature: added bastion.sh script

--- 20 - jenkins-terraform-infra/20-bastion/main.tf
+++ 20 - jenkins-terraform-infra/20-bastion/main.tf
@@ -1,10 +1,11 @@
-# subnet_id is the AWS Subnet ID where this EC2 instance will be launched.
-# Its value comes from local.public_subnet_id, e.g., "subnet-aaa111".
-# Choosing a public subnet allows internet access; private subnet keeps it internal.
 resource "aws_instance" "bastion" {
   ami = local.ami_id
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
+  # subnet_id is the AWS Subnet ID where this EC2 instance will be launched.
+  # Its value comes from local.public_subnet_id, e.g., "subnet-aaa111".
+  # Choosing a public subnet allows internet access; private subnet keeps it internal.
+
   subnet_id = local.pubic_subnet_id
   iam_instance_profile = "bastion_profile"
   root_block_device {
