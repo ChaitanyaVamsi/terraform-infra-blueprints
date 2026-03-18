ecf45a62dc463b2912a30a6394e1f95ba79f569f
chaitanya <vamsi.blaze1@gmail.com>
2026-03-18 10:02:35 +0530

    feature: added main.tf

---
+++ 20 - jenkins-terraform-infra/70-acm/main.tf
@@ -0,0 +1,12 @@
+resource "aws_acm_certificate" "robboshop" {
+  domain_name = "*.${var.domain_name}"
+  validation_method = "DNS"
+
+  tags = merge(local.common_tags,{
+    Name = "local.common_name_suffix"
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
