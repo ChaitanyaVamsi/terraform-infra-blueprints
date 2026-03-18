cd8937d63acc687c3e51aa3de9c3634db5ce19e9
chaitanya <vamsi.blaze1@gmail.com>
2026-03-18 10:36:53 +0530

    faeture: added certificate

---
+++ 20 - jenkins-terraform-infra/70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "domain_name" {
+  default = "cloudops.store"
+}
+
+variable "zone_id" {
+  default = "Z103025810WRVMV3MH616"
+}
